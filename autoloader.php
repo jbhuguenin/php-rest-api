12d892b24318153c7527ee0f69de2f92e8423fe2
Huguenin Jean-Baptiste <jeanbaptiste.huguenin@gmail.com>
2017-07-16 11:49:11 +0200

    remove not use

--- autoloader.php
+++ autoloader.php
@@ -1,26 +1,16 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: jean-baptistehuguenin
- * Date: 16/07/2017
- * Time: 11:41
- */
 
 spl_autoload_register(function ($class) {
 
     $prefix = 'Rest\\';
     $base_dir = __DIR__ . '/src/Rest/';
 
-    $len = strlen($prefix);
-    if (strncmp($prefix, $class, $len) !== 0) {
-        return;
-    }
-
-    $relative_class = substr($class, $len);
+    $relative_class = substr($class, strlen($prefix));
 
     $file = $base_dir . str_replace('\\', '/', $relative_class) . '.php';
 
     if (file_exists($file)) {
         require $file;
     }
+
 });
